42158af3d2a9704a3325c8a4af6ecba867746a3c
Erwin Saputra <eru.saputra@gmail.com>
2020-07-05 16:19:18 +0700

    Add manual control and throttling

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -8,6 +8,7 @@ String motion;
 int val = 0;
 int prevVal = 0;
 int arm = 0;
+int relay = 0;
 
 const int relayPin = D1;
 const int inputPin = D7;
@@ -26,14 +27,15 @@ void reconnect()
   {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
-    String clientId = "ESP8266Client-";
+    String clientId = "emot-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str()))
     {
       Serial.println("connected");
       // once connected `resubscribe`
-      client.subscribe("emot/control");
+      client.subscribe("emot/control"); // arming
+      client.subscribe("emot/manual_control"); // manual-controller
     }
     else
     {
@@ -57,6 +59,16 @@ void callback(char* topic, byte* payload, unsigned int length) {
       // un-arm
       arm = 0;
       Serial.println("Unarming Relay");
+    }    
+  } else if (strncmp(topic, "emot/manual_control", length) == 0) {
+    if (strncmp((char *)payload, "1", length) == 0) {
+      // arm
+      relay = 1;
+      Serial.println("Manual On");
+    } else {
+      // un-arm
+      relay = 0;
+      Serial.println("Unarming Off");
     }
   }
   Serial.print(topic);
@@ -94,43 +106,53 @@ void setup()
   client.setCallback(callback);
 }
 
+int buttonState; 
+int lastButtonState = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
 void loop()
 {
-  // mqtt
   if (!client.connected())
   {
     reconnect();
   }
   client.loop();
 
-  // only actuate to relay if `arm` is 1
-
-  // PIR motion sensor
+  // PIR motion sensor control relay
   val = digitalRead(inputPin);
-  if (val == 0) {
-    // val 0 means no motion
-    digitalWrite(ledPin, HIGH);
-    if (arm == 1) {
-      digitalWrite(relayPin, LOW);
-    }
-  } else {
-    // val 1 means there's motion
-    digitalWrite(ledPin, LOW);
-    if (arm == 1) {
-      digitalWrite(relayPin, HIGH);
-    }    
+  // if the sensor state is changed, due to noise or sensor trigger
+  if (val != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+      // whatever the reading is at, it's been there for longer than the debounce
+      // delay, so take it as the actual current state:    
+      if (val != buttonState) {
+        buttonState = val;
+        if (buttonState == 0) {
+          Serial.println("Motion end");
+          client.publish("emot/motion_event", "0");
+          relay = 0;
+        } else {
+          relay = 1;
+          Serial.println("Motion start");
+          client.publish("emot/motion_event", "1");
+        }        
+      }
   }
 
-  // publish motion delta 
-  if (val != prevVal) {
-    if (val == 1) {
-      Serial.println("Motion started!");
-      client.publish("emot/motion_event", "1");
-    } else {
-      Serial.println("Motion end");
-      client.publish("emot/motion_event", "0");
-    }
-    prevVal = val;
+  // relay trigger - this also can be overriden by the MQTT `emot/manual_control`
+  if (relay == 1) {
+      digitalWrite(ledPin, LOW);
+      if (arm == 1) {
+        digitalWrite(relayPin, HIGH);
+      }    
+  } else {
+      digitalWrite(ledPin, HIGH);
+      if (arm == 1) {
+        digitalWrite(relayPin, LOW);
+      }
   }
 
   // publish motion state - but throttled to be in 250ms interval
@@ -138,8 +160,9 @@ void loop()
   if (currentMillis - lastMeasurement > 250) {   
     lastMeasurement = millis(); 
     char buffer[10];
-    sprintf(buffer, "%d", val);
+    sprintf(buffer, "%d", buttonState);
     client.publish("emot/motion_state", buffer);
-    //Serial.println(buffer);
   }
+
+  lastButtonState = val;
 }
